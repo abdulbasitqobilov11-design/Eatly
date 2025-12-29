669eda074f708736c53f7dd1686a76af1c223505
Abdulbasit <abdulbasit@gmail.com>
2025-12-29 23:36:25 +0500

    qachon

--- js/global.js
+++ js/global.js
@@ -2,3 +2,6 @@ const loading=document.querySelector(".loading");
 window.addEventListener("load", ()=>{
   loading.classList.add("loading-hidden")
 })
+useEffect(() => {
+  fetchData().finally(() => setLoading(false));
+}, []);
