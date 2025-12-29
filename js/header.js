3896a55e6fd65012187d5414c037380c1145a475
Abdulbasit <abdulbasit@gmail.com>
2025-12-29 23:33:17 +0500

    negs

--- js/header.js
+++ js/header.js
@@ -7,10 +7,7 @@ const toggle = document.querySelector('.dropdown-toggle');
 toggle.addEventListener('click', () => {
   dropdown.classList.toggle('active');
 });
-const loading=document.querySelector(".loading");
-window.addEventListener("load", ()=>{
-  loading.classList.add("loading-hidden")
-})
+
 
 
 
