d2fc5dc855d4f61ee1ef3131ff3e5d05a90a8d40
Abdulbasit <abdulbasit@gmail.com>
2025-12-30 17:20:46 +0500

    kal

--- js/app.js
+++ js/app.js
@@ -7,7 +7,10 @@ const data = {
     "Eatly help you set saving goals, earn cash back offers. Go to disclaimer for more details and get paychecks up to two days early. Get a $20 bonus.",
   buttons: ["Get Started", "Go Pro"],
   rating: "4900+",
-  image: "../assets/images/home/hero.png",   
+  image:{
+    src: "../assets/images/home/hero.png",
+    alt: "Hero"
+  },    
   logo: {
     src: "../assets/images/home/truspilot.png",      
     alt: "Trustpilot"
